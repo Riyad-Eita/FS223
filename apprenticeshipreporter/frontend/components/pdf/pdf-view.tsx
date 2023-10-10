99d1eb9bafdc1cdff77700ed84cfac045674b7ce
StevoMc <stevenmc59@gmail.com>
2023-10-11 00:41:45 +0200

    6 (generated by useblackbox.ai )

---
+++ apprenticeshipreporter/frontend/components/pdf/pdf-view.tsx
@@ -0,0 +1,44 @@
+import { createPdf } from "@/lib/pdf";
+import * as React from "react";
+
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { useUser } from "@/hooks/use-actions";
+import { UserProfileType } from "@/types";
+
+const PdfView = ({ ...user }: UserProfileType) => {
+	const [pdfBytes, setPdfBytes] = React.useState<Uint8Array>(
+		new Uint8Array([])
+	);
+
+	React.useEffect(() => {
+		async function fetchPdfBytes() {
+			const bytes = await createPdf(user);
+			setPdfBytes(bytes);
+		}
+		fetchPdfBytes();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+	return (
+		<div className="max-w-[40vw]">
+			<Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+				{pdfBytes instanceof Uint8Array && (
+					<div className="inset-0 overflow-hidden h-full w-full min-w-24 min-h-24">
+						<Viewer
+							theme={"dark"}
+							fileUrl={pdfBytes}
+							plugins={[defaultLayoutPluginInstance]}
+						/>
+					</div>
+				)}
+			</Worker>
+		</div>
+	);
+};
+
+export default PdfView;
--- apprenticeshipreporter/frontend/next.config.js
+++ apprenticeshipreporter/frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	experimental: {
+		serverActions: true,
+		esmExternals: "loose",
+	},
+	webpack: (config) => {
+		config.externals = [...config.externals, { canvas: "canvas" }];
+		return config;
+	},
 };
 
 module.exports = nextConfig;
