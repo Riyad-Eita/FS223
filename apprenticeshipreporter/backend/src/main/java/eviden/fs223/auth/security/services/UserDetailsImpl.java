1e5f004c187273dcaf9e91a393b9bd24f8afc80b
StevoMc <stevenmc59@gmail.com>
2023-10-12 14:49:49 +0200

    fix

--- apprenticeshipreporter/backend/src/main/java/eviden/fs223/auth/security/services/UserDetailsImpl.java
+++ apprenticeshipreporter/backend/src/main/java/eviden/fs223/auth/security/services/UserDetailsImpl.java
@@ -14,85 +14,84 @@ import eviden.fs223.auth.models.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class UserDetailsImpl implements UserDetails {
-  private static final long serialVersionUID = 1L;
 
-  private Long id;
+    private static final long serialVersionUID = 1L;
 
-  private String username;
+    private Long id;
 
-  private String email;
+    private String email;
 
-  @JsonIgnore
-  private String password;
+    @JsonIgnore
+    private String password;
 
-  private Collection<? extends GrantedAuthority> authorities;
+    private Collection<? extends GrantedAuthority> authorities;
 
-  public UserDetailsImpl(Long id, String username, String email, String password) {
-    this.id = id;
-    this.username = username;
-    this.email = email;
-    this.password = password;
-  }
-
-  public static UserDetailsImpl build(User user) {
-
-    return new UserDetailsImpl(
-        user.getId(),
-        user.getFirstname(),
-        user.getEmail(),
-        user.getPassword());
+    public UserDetailsImpl(Long id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
     }
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return authorities;
-  }
+    public static UserDetailsImpl build(User user) {
 
-  public Long getId() {
-    return id;
-  }
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getEmail(),
+                user.getPassword());
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
 
-  @Override
-  public String getPassword() {
-    return password;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  @Override
-  public String getUsername() {
-    return username;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o)
-      return true;
-    if (o == null || getClass() != o.getClass())
-      return false;
-    UserDetailsImpl user = (UserDetailsImpl) o;
-    return Objects.equals(id, user.id);
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
 }
